0aa23afa826d4aa0953344a13024bef0d69027ca
Lechrond <chinnot@qq.com>
2019-10-18 21:53:12 +0800

    配置gulp相关文件并测试

---
+++ xfz/front/gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require("gulp");
+var cssnano = require("gulp-cssnano");
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var bs = require("browser-sync").create();
+
+var path = {
+    'html':'./templates/**/',
+    'css': './src/css/',
+    'js': './src/js/',
+    'images': './src/images/',
+    'css_dist': './dist/css/',
+    'js_dist': './dist/js/',
+    'images_dist': './dist/images/'
+};
+
+//处理html文件的任务
+gulp.task("html", function () {
+    gulp.src(path.html + "*.html")
+        .pipe(bs.stream())
+});
+
+//定义一个css任务
+gulp.task("css", function () {
+    gulp.src(path.css + "*.css")
+        .pipe(cssnano())
+        .pipe(rename({"suffix": ".min"}))
+        .pipe(gulp.dest(path.css_dist))
+        .pipe(bs.stream())
+});
+
+//定义一个js任务
+gulp.task('js', function () {
+    gulp.src(path.js + "*.js")
+        .pipe(concat('index.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.js_dist))
+        .pipe(bs.stream())
+});
+
+//定义处理图片的任务
+gulp.task('image', function () {
+    gulp.src(path.images + "*.*")
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest(path.images_dist))
+        .pipe(bs.stream())
+});
+
+//定义监听文件修改的任务
+gulp.task("watch", function () {
+    gulp.watch(path.html + '*.html', ["html"]);
+    gulp.watch(path.css + '*.css', ["css"]);
+    gulp.watch(path.js + '*.js', ["js"]);
+    gulp.watch(path.images + '*.*', ["images"]);
+});
+
+//初始化browser-sync的任务
+gulp.task("bs", function () {
+    bs.init({
+        'server': {
+            'baseDir': './'
+        }
+    });
+});
+
+//创建一个默认的任务
+gulp.task("default", ['bs', 'watch']);
